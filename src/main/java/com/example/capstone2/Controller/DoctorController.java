42fb109c430af967121f3bdcd96d268a0eb9da24
reemaalsheikh <Reemaalsheikh1@gmail.com>
2024-08-19 09:03:38 +0300

    Add files via upload

---
+++ src/main/java/com/example/capstone2/Controller/DoctorController.java
@@ -0,0 +1,54 @@
+package com.example.capstone2.Controller;
+
+import com.example.capstone2.Api.ApiResponse;
+import com.example.capstone2.Model.Doctor;
+import com.example.capstone2.Service.DoctorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/doctor")
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    // Get all the Doctors
+    @GetMapping("/get")
+    public ResponseEntity getAllDoctors() {
+        return ResponseEntity.status(200).body(doctorService.getAlldoctors());
+    }
+
+    // Add new Doctor
+    @PostMapping("/add")
+    public ResponseEntity addNewDoctor(@Valid @RequestBody Doctor doctor, Errors errors) {
+        if (errors.hasErrors()) {
+            String message =errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        doctorService.addDoctor(doctor);
+        return ResponseEntity.status(200).body(new ApiResponse("Doctor Successfully added!"));
+    }
+
+    // Update Doctors
+    @PutMapping("/update/{doctorId}")
+    public  ResponseEntity updateDoctor(@PathVariable Integer doctorId,@Valid @RequestBody Doctor doctor , Errors errors){
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        doctorService.updateDoctor(doctorId,doctor);
+        return ResponseEntity.status(200).body(new ApiResponse("Doctor Successfully updated!"));
+    }
+
+    // Delete Doctors
+    @DeleteMapping("/delete/{doctorId}")
+    public ResponseEntity deleteDoctors(@PathVariable Integer doctorId){
+        doctorService.deleteDoctor(doctorId);
+        return ResponseEntity.status(200).body(new ApiResponse("Doctor Successfully deleted!"));
+    }
+
+
+}
